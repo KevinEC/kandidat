ff16bc1519108bf43f8b750100c04afd539dd23d
Kevin Evans <k.evans.c@gmail.com>
2019-03-04 16:02:05 +0100

    changed file path in resources.h

--- include/Resources.h
+++ include/Resources.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "cinder/CinderResources.h"
+#include "../cinder_0.9.1_vc2013/include/cinder/CinderResources.h"
 
 //#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
 
